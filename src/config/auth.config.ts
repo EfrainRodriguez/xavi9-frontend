15e72d075aadd9b64898cf7f88db82c1b3e63387
EfrainRodriguez <efrainrg92@gmail.com>
2023-09-09 18:17:32 -0500

    adding process env

--- src/config/auth.config.ts
+++ src/config/auth.config.ts
@@ -1,7 +1,7 @@
 export const msalConfig = {
     auth: {
-        clientId: import.meta.env.VITE_AZURE_ACTIVE_DIRECTORY_CLIENT_ID || "00000000-0000-0000-0000-000000000000",
-        authority: `https://login.microsoftonline.com/${import.meta.env.VITE_AZURE_ACTIVE_DIRECTORY_TENANT_ID || "00000000-0000-0000-0000-000000000000"}`,
+        clientId: process.env.VITE_AZURE_ACTIVE_DIRECTORY_CLIENT_ID || "00000000-0000-0000-0000-000000000000",
+        authority: `https://login.microsoftonline.com/${process.env.VITE_AZURE_ACTIVE_DIRECTORY_TENANT_ID || "00000000-0000-0000-0000-000000000000"}`,
         redirectUri: "/"
     },
     cache: {
